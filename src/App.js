05c77d0c5cc048bb84d443c62a9d507cb2e2f402
Abramov-Alex <abram4ek@rambler.ru>
2021-03-15 18:28:16 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+
+// class SwapiService {
+//     async getResource() {
+//         const res = await fetch(url);
+//         if (res.ok === false) {
+//             console.log(`ERROR ${url}, received ${res.status}`)
+//         }
+//         const body = await res.json();
+//         return body;
+//     }
+//
+//     getAllPeople() {
+//         return this.getResource(`https://swapi.dev/api/people/13242134/`)
+//     }
+//     getPerson(id) {
+//         return this.getResource(`https://swapi.dev/api/people/13242134/`)
+//     }
+// }
+//
+// const swapi = new SwapiService();
+// swapi.getAllPeople().then((body) => {
+//     console.log(body)
+// })
+
+// getResource('https://swapi.dev/api/people/13242134/')
+//     .then((body) => {
+//         console.log(body);
+//     })
+//     .catch((err) => {
+//         console.log('Fetch error is', err);
+//     })
+
+// fetch('https://swapi.dev/api/people/1/')
+//     .then((res) => {
+//         return res.json();
+//     })
+//     .then((body) => {
+//         console.log(body)
+//     })
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      1
+    </>
   );
 }
 
